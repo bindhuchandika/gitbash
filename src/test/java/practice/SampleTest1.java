fee61528afe0f05cb15e153e242e9a17241d8e65
bindhuchandika <121595123+bindhuchandika@users.noreply.github.com>
2023-02-05 14:25:52 +0530

    Update SampleTest1.java

--- src/test/java/practice/SampleTest1.java
+++ src/test/java/practice/SampleTest1.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
-
+//omg
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SampleTest1 {
